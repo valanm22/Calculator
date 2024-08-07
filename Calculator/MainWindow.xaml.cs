13337f8d5740bf85092e75448c1bee4e5bed8930
valanm22 <valanm22@gmail.com>
2024-08-07 02:14:52 -0700

    Add calculation logic

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,7 +17,11 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double _currentValue = 0;
+        private decimal _currentValue = 0;
+        private decimal _previousValue = 0;
+        private string _operation = "";
+        private bool _isNew = true;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,7 +30,86 @@ namespace Calculator
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            Display.Text = Display.Text + button.Content.ToString();
+            String content = button.Content.ToString();
+
+            if (char.IsDigit(content, 0) || content == ".")
+            {
+                if (_isNew)
+                {
+                    Display.Text = content == "." ? "0." : content;
+                    _isNew = false;
+                }
+                else
+                {
+                    Display.Text += content;
+                }
+                _currentValue = decimal.Parse(Display.Text);
+            }
+            else
+            {
+                switch (content)
+                {
+                    case "AC":
+                        _currentValue = 0;
+                        _previousValue = 0;
+                        _operation = "";
+                        Display.Text = "0";
+                        _isNew = true;
+                        break;
+
+                    case "+/-":
+                        _currentValue = -_currentValue;
+                        Display.Text = _currentValue.ToString();
+                        break;
+
+                    case "%":
+                        _currentValue /= 100;
+                        Display.Text = _currentValue.ToString();
+                        break;
+
+                    case "=":
+                        Calculate();
+                        _operation = "";
+                        break;
+
+                    default:
+                        if (!string.IsNullOrEmpty(_operation))
+                        {
+                            Calculate();
+                        }
+                        _previousValue = _currentValue;
+                        _operation = content;
+                        _isNew = true;
+                        break;
+                }
+            }
+        }
+        private void Calculate()
+        {
+            switch (_operation)
+            {
+                case "+":
+                    _currentValue = _previousValue + _currentValue;
+                    break;
+                case "-":
+                    _currentValue = _previousValue - _currentValue;
+                    break;
+                case "×":
+                    _currentValue = _previousValue * _currentValue;
+                    break;
+                case "÷":
+                    if (_currentValue != 0)
+                    {
+                        _currentValue = _previousValue / _currentValue;
+                    }
+                    else
+                    {
+                        Display.Text = "undefined";
+                    }
+                    break;
+            }
+
+            Display.Text = _currentValue.ToString();
         }
     }
 }
